50455c66c76f1f83378810baadee067582964eaf
Valer8867 <valer8867@gmail.com>
2020-06-03 00:35:32 +0300

    allRight

--- SOos/main.cpp
+++ SOos/main.cpp
@@ -34,6 +34,7 @@ using namespace std;
 string multiply(string num1, string num2);
 std::string sum_strings(const std::string& a, const std::string& b);
 string divide_strings(string a, string b);
+string findDiff(string str1, string str2);
 
 
 
@@ -47,19 +48,19 @@ string floorSqrt(string x)
     string start = "1", end = x, ans;
     while (start <= end)
     {
-        string mid = (start + end) / 2;
+        string mid = divide_strings(sum_strings(start, end), "2");
 
         // If x is a perfect square 
-        if (mid * mid == x)
+        if (multiply(mid,mid) == x)
             return mid;
 
-        if (mid * mid < x)
+        if (multiply(mid, mid) < x)
         {
-            start = mid + 1;
+            start = sum_strings(mid, "1");
             ans = mid;
         }
         else 
-            end = mid - 1;
+            end = findDiff(mid, "1");
     }
     return ans;
 }
@@ -75,7 +76,83 @@ int main() {
 
 
 
+bool isSmaller(string str1, string str2)
+{
+    // Calculate lengths of both string 
+    int n1 = str1.length(), n2 = str2.length();
 
+    if (n1 < n2)
+        return true;
+    if (n2 > n1)
+        return false;
+
+    for (int i = 0; i < n1; i++)
+    {
+        if (str1[i] < str2[i])
+            return true;
+        else if (str1[i] > str2[i])
+            return false;
+    }
+    return false;
+}
+
+// Function for finding difference of larger numbers 
+string findDiff(string str1, string str2)
+{
+    // Before proceeding further, make sure str1 
+    // is not smaller 
+    if (isSmaller(str1, str2))
+        swap(str1, str2);
+
+    // Take an empty string for storing result 
+    string str = "";
+
+    // Calculate lengths of both string 
+    int n1 = str1.length(), n2 = str2.length();
+    int diff = n1 - n2;
+
+    // Initially take carry zero 
+    int carry = 0;
+
+    // Traverse from end of both strings 
+    for (int i = n2 - 1; i >= 0; i--)
+    {
+        // Do school mathematics, compute difference of 
+        // current digits and carry 
+        int sub = ((str1[i + diff] - '0') -
+            (str2[i] - '0') -
+            carry);
+        if (sub < 0)
+        {
+            sub = sub + 10;
+            carry = 1;
+        }
+        else
+            carry = 0;
+
+        str.push_back(sub + '0');
+    }
+
+    // subtract remaining digits of str1[] 
+    for (int i = n1 - n2 - 1; i >= 0; i--)
+    {
+        if (str1[i] == '0' && carry)
+        {
+            str.push_back('9');
+            continue;
+        }
+        int sub = ((str1[i] - '0') - carry);
+        if (i > 0 || sub > 0) // remove preceding 0's 
+            str.push_back(sub + '0');
+        carry = 0;
+
+    }
+
+    // reverse resultant string 
+    reverse(str.begin(), str.end());
+
+    return str;
+}
 
 std::string sum_strings(const std::string& a, const std::string& b) {
     std::string res;
