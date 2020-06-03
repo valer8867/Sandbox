7d86088d0eabf6251d7205e3c327f23a8170021b
Valer8867 <valer8867@gmail.com>
2020-06-03 15:56:03 +0300

    add anwer

--- SOos/main.cpp
+++ SOos/main.cpp
@@ -28,16 +28,41 @@ struct stage_hash
 std::vector<int> slide_puzzle(const Table& arr) {
     int i = 0;
     int j = 0;
-    for (auto sizeR = arr.size(), sizeC = arr[0].size(); i < sizeR; ++i) {
-        for (; j < sizeC; ++j)
+    auto sizeR = arr.size(), sizeC = arr[0].size();
+    for (; i < sizeR; ++i) {
+        for (j = 0; j < sizeC; ++j)
             if (arr[i][j] == 0) goto endLoop;
     }
 endLoop:
+    /////
+    auto answer = Table(sizeR, vector<int>(sizeC, 0));
+    size_t counter = 1;
+    for (auto& i : answer) {
+        for (auto& j : i) {
+            j = counter++;
+        }
+    }
+    answer.back().back() = 0;
+    /////
     unordered_set<Stage, stage_hash> tables;
     tables.insert({ arr, {i, j} });
-
+    Table tmp;
     for (auto& table : tables) {
-        //move_one();
+        if (i > 0) {
+            tmp = arr;
+            std::swap(tmp[i][j], tmp[i - 1][j]);
+
+            tables.insert({ tmp, {i - 1, j} });
+        }
+        if (i < sizeR - 1) {
+
+        }
+        if (j > 0) {
+
+        }
+        if (j < sizeC) {
+
+        }
 
     }
     return std::vector<int>();
@@ -51,6 +76,12 @@ int main() {
     vector<vector<int>> v = { { 4,1,3 },
                             { 2,8,0 },
                             { 7,6,5 } };
+    vector<vector<int>> z = { { 4,1,3 },
+                            { 2,8,0 },
+                            { 7,6,5 } };
+    //cout << (Stage{ v, { 1, 2 } } == Stage{ z, { 1, 2 } });
+    //cout << (z == v);
+    slide_puzzle(v);
     
     
 }
