901dd0ddac6681d3292ccea99be07b8e25b78c88
Valer8867 <valer8867@gmail.com>
2020-06-03 12:53:31 +0300

    use idea of unordered_set

--- SOos/main.cpp
+++ SOos/main.cpp
@@ -4,18 +4,30 @@
 #include <vector>
 #include <numeric>
 #include <algorithm>
-#include <string_view>
+#include <unordered_set>
 
 using namespace std;
+using Table = std::vector<std::vector<int>>;
 
+std::vector<int> slide_puzzle(const Table& arr)
+{
+    unordered_set<Table> tables;
+    tables.insert(arr);
+    for (auto& table : tables) {
 
-
+    }
+    return std::vector<int>();
+}
 
 
 
 int main() {
-   
-    cout;
+
+
+    vector<vector<int>> v = { { 4,1,3 },
+                            { 2,8,0 },
+                            { 7,6,5 } };
+    
     
 }
 
