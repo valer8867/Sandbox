ea3d82aaa833470232bc8e0da49330d4f600ddef
Valer8867 <valer8867@gmail.com>
2020-06-03 13:44:12 +0300

    no hash function

--- SOos/main.cpp
+++ SOos/main.cpp
@@ -8,12 +8,32 @@
 
 using namespace std;
 using Table = std::vector<std::vector<int>>;
+using Stage = std::pair<Table, std::pair<int, int>>;
+
+
+struct stage_hash
+{
+    template <class T1, class T2>
+    std::size_t operator () (std::pair<T1, T2> const& pair) const {
+        return std::hash<T1>()(pair.first);
+    }
+};
 
 std::vector<int> slide_puzzle(const Table& arr)
 {
-    unordered_set<Table> tables;
-    tables.insert(arr);
+    int i = 0;
+    int j = 0;
+    for (auto sizeR = arr.size(), sizeC = arr[0].size(); i < sizeR; ++i) {
+        for (; j < sizeC; ++j)
+            if (arr[i][j] == 0) goto endLoop;
+    }
+endLoop:
+    
+    unordered_set<Stage, stage_hash> tables;
+    tables.insert({ arr, {i, j} });
+
     for (auto& table : tables) {
+        //move_one();
 
     }
     return std::vector<int>();
